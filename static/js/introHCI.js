f4852dae36147f74019fd2f5851999db1d4086b6
annsudhart <sudharta.andrea@gmail.com>
2019-01-24 10:33:18 -0800

    up to slide 47

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -3,6 +3,7 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+	projectClick();
 })
 
 /*
@@ -10,9 +11,25 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript has taken control");
+		$("#testjs").toggleClass("active");
+
+		setTimeout(function(){$('.jumbotron h1').animate({"opacity" : 0}, 2000);	}, 500);
 	});
 
+  $('.project').click(projectClick);
+
+}
+
+function projectClick() {
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+  console.log("initialized");
+  var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+	if (description.length == 0) {
+	  $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+	} else {
+	  description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+	}
 }
